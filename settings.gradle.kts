57285722b82bda42c37568d5c301aacc6e307107
trickstersih <trickstersih@gmail.com>
2026-08-28 21:09:41 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,37 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "imantra"
+include(":app")
+include(":core-domain")
+include(":core-platform-api")
+include(":core-protocol")
+include(":platform-android")
+include(":engine-discovery")
+include(":engine-mesh")
+include(":engine-speech")
+include(":feature-ptt")
+include(":feature-emergency")
+
+ 
