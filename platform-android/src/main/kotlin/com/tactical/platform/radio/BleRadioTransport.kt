15f19102f10125515f60a41b6db47a86d38204ab
Rayyan Ahmed <rayyan8303073@gmail.com>
2026-09-19 20:01:20 +0530

    Fix BLE transport coroutine launch import

--- platform-android/src/main/kotlin/com/tactical/platform/radio/BleRadioTransport.kt
+++ platform-android/src/main/kotlin/com/tactical/platform/radio/BleRadioTransport.kt
@@ -14,6 +14,7 @@ import com.tactical.platform.api.radio.RawPacket
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
