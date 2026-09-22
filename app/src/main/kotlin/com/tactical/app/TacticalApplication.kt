1fdab80a61c214fe2ba9ad3a59e163a24d458d7e
Rayyan Ahmed <rayyan8303073@gmail.com>
2026-09-22 23:24:12 +0530

    Clean unused application import

--- app/src/main/kotlin/com/tactical/app/TacticalApplication.kt
+++ app/src/main/kotlin/com/tactical/app/TacticalApplication.kt
@@ -5,7 +5,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
-import java.io.File
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
