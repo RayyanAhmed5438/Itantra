643c76aaebf91902a336fa5f8cb05ba9f62afb1b
Rayyan Ahmed <rayyan8303073@gmail.com>
2026-09-22 23:11:29 +0530

    Clean up message selection imports

--- app/src/main/kotlin/com/tactical/app/ui/screens/MessagesScreen.kt
+++ app/src/main/kotlin/com/tactical/app/ui/screens/MessagesScreen.kt
@@ -2,7 +2,6 @@ package com.tactical.app.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
