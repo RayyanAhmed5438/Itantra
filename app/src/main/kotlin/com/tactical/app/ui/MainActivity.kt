99116196938ed11a800823a1ed0b4551b51143f1
Rayyan Ahmed <rayyan8303073@gmail.com>
2026-09-26 23:06:20 +0530

    Use blue app chrome on Settings

--- app/src/main/kotlin/com/tactical/app/ui/MainActivity.kt
+++ app/src/main/kotlin/com/tactical/app/ui/MainActivity.kt
@@ -90,7 +90,7 @@ class MainActivity : ComponentActivity() {
                 val state by viewModel.uiState.collectAsState()
                 var selectedTab by remember { mutableIntStateOf(0) }
                 var showSettings by remember { mutableStateOf(false) }
-                val blueTheme = !showSettings
+                val blueTheme = true
 
                 Scaffold(
                         topBar = {
